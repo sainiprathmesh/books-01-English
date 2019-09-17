c269cc0259235c4f7c69322354e967145c9f3fd9
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-09-17 20:31:45 +0530

    input taken

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,18 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        final Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine().trim();
+        Book[] books = new Book[n];
+        for (int i = 0; i < n; i++) {
+            String bname = sc.nextLine().trim();
+            String baname = sc.nextLine().trim();
+            String isbn = sc.nextLine().trim();
+            books[i] = new Book(bname, baname, isbn);
+        }
     }
 }
