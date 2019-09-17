9827e24e24a39ada504b35b87f22ae716e980eee
Prathmesh Saini <prathmesh.saini_cs18@gla.ac.in>
2019-09-17 20:26:31 +0530

    getter defined

--- src/main/Book.java
+++ src/main/Book.java
@@ -4,4 +4,16 @@ public class Book {
     private String bname;
     private String baname;
     private String isbn;
+
+    public String getBname() {
+        return bname;
+    }
+
+    public String getBaname() {
+        return baname;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
 }
